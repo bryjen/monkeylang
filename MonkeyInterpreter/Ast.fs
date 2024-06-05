c50b6458edc3df33207c7af24833a1fe68acce7a
Bryan <miguelbryancarlo3434@gmail.com>
2024-06-05 16:07:34 -0400

    Reformats Ast.fs

--- MonkeyInterpreter/Ast.fs
+++ MonkeyInterpreter/Ast.fs
@@ -3,87 +3,89 @@ namespace MonkeyInterpreter
 open System
 open MonkeyInterpreter.Token
 
-[<AutoOpen>]
-module Ast =
     
-    ///
-    type Identifier =
-        { Token: Token // 'TokenType.IDENT' token
-          Value: string }
+///
+type Identifier =
+    { Token: Token // 'TokenType.IDENT' token
+      Value: string }
+with
+    member this.TokenLiteral() = this.Value
         
-        member this.TokenLiteral() = this.Value
-        
-        static member FromToken token =
-            if token.Type <> TokenType.IDENT then
-                let errorMsg = $"Expected token type of \"IDENT\", but received \"{TokenType.ToCaseString token.Type}\""
-                raise (ArgumentException(errorMsg))
-            else
-                { Token = token; Value = token.Literal }
-            
 
-    ///
-    type Node =
-        | Statement of Statement
-        | Expression of Expression
+///
+type Node =
+    | Statement of Statement
+    | Expression of Expression
+with
+    member this.TokenLiteral() =
+        match this with
+        | Statement statement -> statement.TokenLiteral() 
+        | Expression expression -> expression.TokenLiteral() 
+    
+    
+///
+and Statement =
+    | LetStatement of LetStatement
+with
+    member this.TokenLiteral() =
+        match this with
+        | LetStatement letStatement -> letStatement.TokenLiteral()
         
-        member this.TokenLiteral() =
-            match this with
-            | Statement statement -> statement.TokenLiteral() 
-            | Expression expression -> expression.TokenLiteral() 
+    member this.StatementNode() =
+        match this with
+        | LetStatement letStatement -> letStatement.StatementNode()
+        
+    /// <inheritdoc/>
+    override this.ToString() =
+        match this with
+        | LetStatement letStatement -> letStatement.ToString()
+        
+    
+///
+and Expression =
+    | Something
+with
+    member this.TokenLiteral() =
+        match this with
+        | Something -> "todo"
+        
+    member this.ExpressionNode() =
+        failwith "todo"
+        
+    /// <inheritdoc/>
+    override this.ToString() =
+        // TODO: Update when expression actually does something
+        "EXPRESSION"
+    
+    
+///
+and LetStatement =
+     { Token: Token // 'TokenType.LET' token 
+       Name: Identifier 
+       Value: Expression }
+with
+    member this.TokenLiteral() = this.Token.Literal
+        
+    member this.StatementNode() =
+        failwith "todo"
+        
+    /// <inheritdoc/>
+    override this.ToString() =
+        $"\"let {this.Name.Value} = {this.Value}\""
+    
+        
+///
+type Program =
+    { Statements: Statement list }
+with
+    member this.TokenLiteral() =
+        match this.Statements with
+        | firstStatement :: _ -> firstStatement.TokenLiteral()
+        | [] -> ""
         
         
-    ///
-    and Statement =
-        | LetStatement of LetStatement
-    with
-        override this.ToString() =
-            match this with
-            | LetStatement letStatement -> letStatement.ToString() 
-        
-        member this.TokenLiteral() =
-            match this with
-            | LetStatement letStatement -> letStatement.TokenLiteral()
-            
-        member this.StatementNode() =
-            failwith "todo"
-            
-        
-    ///
-    and Expression =
-        | Something
-    with
-        override this.ToString() =
-            // TODO: Update when expression actually does something
-            "EXPRESSION"
-        
-        member this.TokenLiteral() =
-            match this with
-            | Something -> "todo"
-            
-        member this.ExpressionNode() =
-            failwith "todo"
-        
-        
-    ///
-    and LetStatement =
-         { Token: Token // 'TokenType.LET' token 
-           Name: Identifier 
-           Value: Expression }
-    with
-        override this.ToString() =
-            $"\"let {this.Name.Value} = {this.Value}\""
-        
-        member this.TokenLiteral() = this.Token.Literal
-            
-        member this.StatementNode() =
-            failwith "todo"
-        
-            
-    ///
-    type Program =
-        { Statements: Statement list }
-        
-        member this.TokenLiteral() =
-            match this.Statements with
-            | firstStatement :: _ -> firstStatement.TokenLiteral()
-            | [] -> ""
+    /// <inheritdoc/>
+    override this.ToString() =
+        this.Statements
+        |> List.map (_.ToString())
+        |> String.concat "\n"
