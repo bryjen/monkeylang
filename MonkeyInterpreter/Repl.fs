d54ca2a0b4920c8e0c3850ecd7dde20b00782286
Bryan <miguelbryancarlo3434@gmail.com>
2024-07-15 19:10:00 -0400

    Changes REPL to display parser from lexer output

--- MonkeyInterpreter/Repl.fs
+++ MonkeyInterpreter/Repl.fs
@@ -6,14 +6,23 @@ module Repl =
     let private Prompt = ">> "
     
     
-    let rec startRepl (stdIn: TextReader) (stOut: TextWriter) =
+    let rec startRepl (stdIn: TextReader) (stdOut: TextWriter) =
         stdout.Write(Prompt) 
         
         match stdIn.ReadLine() with
         | null ->
             ()
         | input ->
-            let tokens = Lexer.parseIntoTokens input
-            tokens |> List.iter stOut.WriteLine
+            let program = Parser.parseProgram input
+            handleParseResults stdOut program
 
         startRepl stdIn stdout
+        
+    and handleParseResults (stdOut: TextWriter) (program: Program) : unit =
+        if program.Errors.Length > 0 then
+            let errorMsgsString = program.Errors
+                                  |> List.map (fun errorMsg -> "- " + errorMsg)
+                                  |> String.concat "\n"
+            stdOut.WriteLine($"The following errors occurred when parsing:\n{errorMsgsString}")
+        else
+            program.Statements |> List.iter (fun stat -> stdOut.WriteLine($"{stat.ToString()}"))
